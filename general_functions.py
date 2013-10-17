9730f411b26eeeb9d5be313521e0680beb6bfc01
jwerdec <joern.werdecker@mpibpc.mpg.de>
2013-10-17 14:12:20 +0200

    bugfix

--- general_functions.py
+++ general_functions.py
@@ -4,4 +4,4 @@ import numpy as np
 gaussian = lambda x, x0, A, w, offset: A * np.exp(- (x-x0)**2/w**2) + offset
 asymw = lambda x, x0, w0, alpha: 2*w0/(1+np.exp(alpha*(x-x0)))
 asymgauss = lambda x, A, x0, w0, alpha, offset : \
-    gaussian(x, A, x0, asymw(x, x0, w0, alpha), offset)
+    gaussian(x, x0, A, asymw(x, x0, w0, alpha), offset)
